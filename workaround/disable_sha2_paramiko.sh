7de1e234710685498c4a59af97ba091f6a4e05be
morFraSh <34887102+morFraSh@users.noreply.github.com>
2024-06-07 01:54:47 +0300

    Update disable_sha2_paramiko.sh

--- workaround/disable_sha2_paramiko.sh
+++ workaround/disable_sha2_paramiko.sh
@@ -1,10 +1,16 @@
-#! /bin/sh
+#! /bin/bash
+MODE=$1
 PATH_PKG_PYTHON=$(python -m pip show paramiko | sed -r -n "s/[Ll]ocation\: (.*)/\1/p")/paramiko/transport.py
 CHECK_DISABLE=$(grep -c "disabled_algorithms=None" $PATH_PKG_PYTHON)
-if [ $CHECK_DISABLE -eq 1 ]; then
-    sed -i "s/disabled_algorithms=None/disabled_algorithms={'pubkeys': \['rsa-sha2-256', 'rsa-sha2-512'\]}/g" $PATH_PKG_PYTHON
-    echo 'Disable SHA2 in paramiko (py venv)'
+
+if [[ $MODE =~ "isable" ]]; then
+   if [ $CHECK_DISABLE -eq 1 ]; then
+      sed -i "s/disabled_algorithms=None/disabled_algorithms={'pubkeys': \['rsa-sha2-256', 'rsa-sha2-512'\]}/g" $PATH_PKG_PYTHON
+   fi
+   echo 'Disable SHA2 in paramiko (py venv)'
 else 
-    sed -i "s/disabled_algorithms={'pubkeys': \['rsa-sha2-256', 'rsa-sha2-512'\]}/disabled_algorithms=None/g" $PATH_PKG_PYTHON
-    echo 'Enable SHA2 in paramiko (py venv)'
+   if [[ $MODE =~ "nable" ]]; then
+      sed -i "s/disabled_algorithms={'pubkeys': \['rsa-sha2-256', 'rsa-sha2-512'\]}/disabled_algorithms=None/g" $PATH_PKG_PYTHON
+      echo 'Enable SHA2 in paramiko (py venv)'
+   fi
 fi
