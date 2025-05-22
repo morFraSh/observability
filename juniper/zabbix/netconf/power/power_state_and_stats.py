1dc577e03ae28ea83207f0cffb8f7e4f8e2530a7
mositos <mikhailbogom@gmail.com>
2025-05-22 19:24:19 +0300

    add get_environment_component_information_lld and get_environment_component_information_elem func for templete zabbix

--- juniper/zabbix/netconf/power/power_state_and_stats.py
+++ juniper/zabbix/netconf/power/power_state_and_stats.py
@@ -4,47 +4,85 @@
 from jnpr.junos import Device
 import jnpr.junos.exception
 from lxml import etree
+#from lxml import html
 #import jxmlease.xmlparser
-import argparse
+from argparse import ArgumentParser, RawTextHelpFormatter
 #import json.decoder
-
-
-parser = argparse.ArgumentParser(add_help = True,
-                                 formatter_class = argparse.RawTextHelpFormatter,
-                                 description = 'Get information on optical trancievers',
-                                 usage = '%(prog)s --host --user --key --timeout --output')
-
-parser.add_argument("--user", "-u", help='username', metavar='')
-parser.add_argument("--key", "-k", help='full path to rsa private key', metavar='')
-parser.add_argument("--host", "-H", help='set host ip address', required=True, metavar='')
-parser.add_argument("--timeout", "-t", help='set host timeout', metavar='')
-args = parser.parse_args()
-
-host_a = args.host
-user_netconf_a = args.user
-ssh_key_a = args.key
-timeout_a = args.timeout
+from json import dumps
 
 
 
-def get_environment_component_information_lld(host, user_netconf, ssh_key, timeout_rpc):
-    """Used when mode=descr lld_set"""
-    json_disc_list = []
-
+def get_environment_component_information_lld(host, user_netconf, ssh_key, timeout_rpc, mode_sat_a):
+    """Used when mode=elem_set"""
+    
+    if mode_sat_a == "on-sat":
+        mode_sat_a = True
+    elif mode_sat_a == "no-sat":
+        mode_sat_a = False
+    else:
+        return "Error mode_sat_a"
+    
+    env_power_all_sat = None
+    json_lld_list = []
+    
     dev = Device(host=host, user=user_netconf, ssh_private_key_file=ssh_key, gather_facts=False, timeout=timeout_rpc)
     dev.open()
     env_power_all = dev.rpc.get_environment_pem_information()
+    if mode_sat_a is True:
+        env_power_all_sat = dev.rpc.get_chassis_environment_pem_satellite_info()
     dev.close()
 
-    return etree.tostring(env_power_all, encoding='unicode')
+    env_power_unit = env_power_all.xpath('//environment-component-item[dc-information/dc-detail/dc-power] | //environment-component-item[dc-information/dc-detail/str-dc-power]')
+    env_power_xpath_name = env_power_unit.xpath('//environment-component-item/name')
+
+    if mode_sat_a is True:
+        env_power_unit_sat = env_power_all_sat.xpath('//environment-component-item[dc-information/dc-detail/dc-power] | //environment-component-item[dc-information/dc-detail/str-dc-power]')
+        env_power_sat_xpath_name = env_power_unit_sat.xpath('//environment-component-item/name')
+        env_power_xpath_name = env_power_xpath_name + env_power_sat_xpath_name
+    for psu_name in env_power_xpath_name:
+        json_lld_list.append('name': str(psu_name))
+
+    return json_lld_list
 
 
 
-def power_exception():
+def get_environment_component_information_elem(host, user_netconf, ssh_key, timeout_rpc, mode_sat_a):
+    """Used when mode=lld_set"""
+    if mode_sat_a == "on-sat":
+        mode_sat_a = True
+    elif mode_sat_a == "no-sat":
+        mode_sat_a = False
+    else:
+        return "Error mode_sat_a"
+    dev = Device(host=host, user=user_netconf, ssh_private_key_file=ssh_key, gather_facts=False, timeout=timeout_rpc)
+
+    dev.open()
+    env_power_all = dev.rpc.get_environment_pem_information()
+    if mode_sat_a is True:
+        env_power_sat = dev.rpc.get_chassis_environment_pem_satellite_info()
+    dev.close()
+
+    if mode_sat_a is True:
+        [env_power_all.insert(0, i) for i in env_power_sat]
+
+    return etree.tostring(env_power_all, encoding='unicode',method='xml',with_tail=False)
+
+
+
+def power_exception(mode):
     try:
-        result = get_environment_component_information_lld(host_a, user_netconf_a, ssh_key_a, timeout_a)
-        if not result == None:
+        if mode == "elem_set":
+            result = get_environment_component_information_elem(host_a, user_netconf_a, ssh_key_a, timeout_rpc, mode_sat_a)
+            if not result == None:
+                return result
+        elif mode == "lld_set":
+            result = get_environment_component_information_lld(host_a, user_netconf_a, ssh_key_a, timeout_rpc, mode_sat_a)
+            ####json formated####
+            result = dumps(result, indent=4, separators=(',', ': '))
             return result
+        else:
+            return "Error: mode generic"
+
 
     except (jnpr.junos.exception.ConnectRefusedError,
             jnpr.junos.exception.ConnectUnknownHostError,
@@ -61,5 +99,22 @@ def power_exception():
 
 
 if __name__ == '__main__':
-    print(power_exception())
+    parser = ArgumentParser(add_help = True,
+                                    formatter_class = RawTextHelpFormatter,
+                                    description = 'Get information on optical trancievers',
+                                    usage = '%(prog)s --host --user --key --timeout --output')
 
+    parser.add_argument("--user", "-u", help='username', metavar='')
+    parser.add_argument("--key", "-k", help='full path to rsa private key', metavar='')
+    parser.add_argument("--host", "-H", help='set host ip address', required=True, metavar='')
+    parser.add_argument("--timeout", "-t", help='set host timeout', metavar='')
+    parser.add_argument("--output", "-o", help='set mode lld_set or elem_set', metavar='')
+    args = parser.parse_args()
+
+    host_a = args.host
+    user_netconf_a = args.user
+    ssh_key_a = args.key
+    timeout_rpc = args.timeout
+    output_a = args.output
+
+    print(power_exception(output_a))
